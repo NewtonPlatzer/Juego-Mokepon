a21ca616184441512b3a281fd384b8d26e93aaf7
Ike Gomber <isaacbermude14@gmail.com>
2023-02-13 19:13:13 -0500

    Código js mokepon

--- script/script.js
+++ script/script.js
@@ -1,23 +1,86 @@
-window.addEventListener('load', iniciarJuego)
+let ataqueJugador
+let ataqueEnemigo
+let mascotaJugador
+let mascotaEnemigo
 
 function iniciarJuego() {
-    let botonMascotaJugador = document.getElementById('boton-mascota');
-
+    let botonMascotaJugador = document.getElementById('boton-mascota')
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+
+    let botonFuego = document.getElementById('boton-fuego')
+    botonFuego.addEventListener('click', ataqueFuego)
+
+    let botonAgua = document.getElementById('boton-agua')
+    botonAgua.addEventListener('click', ataqueAgua)
+
+    let botonTierra = document.getElementById('boton-tierra')
+    botonTierra.addEventListener('click', ataqueTierra)
 }
 
 function seleccionarMascotaJugador() {
-    inputHipodoge = document.getElementById('hipodoge');
-    inputCapipepo = document.getElementById('capipepo');
-    inputRatigueya = document.getElementById('ratigueya');
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+    let spanMascotaJugador = document.getElementById('mascota-jugador')
+    let seleccionarMascota = document.getElementById('seleccionar-mascota')
+
     if (inputHipodoge.checked) {
-        alert("La mascota seleccionada es HIPODOGE");
+        spanMascotaJugador.innerHTML = 'Hipodoge'
     } else if (inputCapipepo.checked) {
-        alert("La mascota seleccionada es CAPIPEPO");
+        spanMascotaJugador.innerHTML = 'Capipepo'
     } else if (inputRatigueya.checked) {
-        alert("La mascota seleccionada es RATIGUEYA");
-    } else {
-        alert("Tienes que seleccionar una mascota");
+        spanMascotaJugador.innerHTML = 'Ratigueya'
     }
+    else {
+        alert('Selecciona una mascota')
+    }
+    seleccionarMascotaEnemigo()
 }
 
+function seleccionarMascotaEnemigo() {
+    let mascotaAleatoria = aleatorio(1, 3)
+    let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
+    if (mascotaAleatoria == 1) { spanMascotaEnemigo.innerHTML = 'Hipodoge' } else if (mascotaAleatoria == 2) { spanMascotaEnemigo.innerHTML = 'Capipepo' } else { spanMascotaEnemigo.innerHTML = 'Ratigueya' }
+}
+
+function ataqueFuego() {
+    ataqueJugador = 'FUEGO'
+    ataqueAleatorioEnemigo()
+}
+
+function ataqueAgua() {
+    ataqueJugador = 'AGUA'
+    ataqueAleatorioEnemigo()
+}
+
+function ataqueTierra() {
+    ataqueJugador = 'TIERRA'
+    ataqueAleatorioEnemigo()
+}
+
+function ataqueAleatorioEnemigo() {
+    let ataqueAleatorio = aleatorio(1, 3)
+    if (ataqueAleatorio == 1) { ataqueEnemigo = 'FUEGO' } else if (ataqueAleatorio == 2) { ataqueEnemigo = 'AGUA' } else { ataqueEnemigo = 'TIERRA' }
+    combate()
+}
+
+function combate() {
+    if (ataqueEnemigo == ataqueJugador) {
+        crearMensaje("EMPATE")
+    } else if (ataqueJugador == 'FUEGO' && ataqueEnemigo == 'TIERRA') {
+        crearMensaje("GANASTE")
+    } else if (ataqueJugador == 'AGUA' && ataqueEnemigo == 'FUEGO') {
+        crearMensaje("GANASTE")
+    } else if (ataqueJugador == 'TIERRA' && ataqueEnemigo == 'AGUA') {
+        crearMensaje("GANASTE")
+    } else { crearMensaje("PERDISTE") }
+}
+function crearMensaje(resultado) {
+    let sectionMensajes = document.getElementById('mensajes')
+    let parrafo = document.createElement('p')
+    parrafo.innerHTML = 'Tu mascota ataca con ' + ataqueJugador + ', la mascota del enemigo ataca con ' + ataqueEnemigo + ' - ' + resultado
+    sectionMensajes.appendChild(parrafo)
+}
+
+function aleatorio(min, max) { return Math.floor(Math.random() * (max - min + 1) + min) }
+window.addEventListener('load', iniciarJuego)
